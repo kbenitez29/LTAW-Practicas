9ca89904039333e7523809006a07432ed36d666e
kbenitez29 <kevin_beni1997@hotmail.com>
2022-02-20 04:08:56 +0100

    avance server

--- P1/tienda.js
+++ P1/tienda.js
@@ -12,26 +12,38 @@ const server = http.createServer((req, res)=>{
     //-- Valores de la respuesta por defecto
     let code = 200;
     let code_msg = "OK";
-    let page = pagina_main;
+    //let page = pagina_main;
 
-    //-- Obtiene los distintos campos de url
+    //-- Obtiene los distintos campos (recursos)(req.url) de la URL (parse) de la solicitud
     let myURL = url.parse(req.url, true);
-    console.log('Recurso:', myURL.pathname);
+    console.log('Recurso solicitado:', myURL.pathname);
+
+    //-- Variable para alamcenar el archivo solicitado
+    let file = ''
+
+    //-- Recurso raiz, devuelve el main 
+    if (url.pathname == '/') {
+        file += 'main.html'
+    // Si no es recurso raiz devuelve la ruta que se pida
+    } else{
+        console.log(url.pathname)
+        file += url.pathname
+    }
 
     //-- Cualquier recurso que no sea la página principal
     //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
-    }
+    // if (url.pathname != '/') {
+    //     code = 404;
+    //     code_msg = "Not Found";
+    //     page = pagina_error;
+    // }
 
     //-- Generar la respusta en función de las variables
     //-- code, code_msg y page
     res.statusCode = code;
     res.statusMessage = code_msg;
     res.setHeader('Content-Type','text/html');
-    res.write(page);
+    res.write(file);
     res.end();
 });
 
